9e5600557548672109c6a66b362c904c70bf50e9
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-06-23 23:43:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -144,6 +144,32 @@
 </template>
 
 <script>
+
+<link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
+<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+<df-messenger
+  project-id="teak-gift-423704-v2"
+  agent-id="0d090277-c7ed-4ff2-9a52-c0b80e02ae46"
+  language-code="en"
+  max-query-length="-1">
+  <df-messenger-chat-bubble
+   chat-title="">
+  </df-messenger-chat-bubble>
+</df-messenger>
+<style>
+  df-messenger {
+    z-index: 999;
+    position: fixed;
+    --df-messenger-font-color: #000;
+    --df-messenger-font-family: Google Sans;
+    --df-messenger-chat-background: #f3f6fc;
+    --df-messenger-message-user-background: #d3e3fd;
+    --df-messenger-message-bot-background: #fff;
+    bottom: 16px;
+    right: 16px;
+  }
+</style>
+
 import Img from "./image";
 import Module from "./imghelper";
 
